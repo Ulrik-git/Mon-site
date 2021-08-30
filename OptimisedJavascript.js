3d05b1d4a22ae0de16e5f8ac7d5f5b9eb4fc57ac
Ulrik <ulrik.edeline@outlook.fr>
2021-08-30 23:01:02 +0200

    Js changes

--- OptimisedJavascript.js
+++ OptimisedJavascript.js
@@ -1,7 +1,7 @@
 function enleverCache() {
-    document.getElementById('cache1').style.cssText = 'display: none';
+    document.getElementById('cache').style.cssText = 'display: none';
 }
 
 function mettreCache() {
-    document.getElementById('cache1').style.cssText = 'display: all';
+    document.getElementById('cache').style.cssText = 'display: all';
 }
